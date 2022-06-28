64a282d0c6b129f7f03506c9dc8908e15117ff7d
Wox <YeeeeRQ@outlook.com>
2022-06-28 22:28:38 +0800

    建构页面底栏结构与样式.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,51 @@
 <template>
-  <div>Hello World</div>
+  <div class="docker">
+    <div class="docker__item docker__item--active">
+      <div class="iconfont icon-home1"></div>
+      <div class="docker__title">首页</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont icon-cart-full"></div>
+      <div class="docker__title">购物车</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont icon-order"></div>
+      <div class="docker__title">订单</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont icon-my"></div>
+      <div class="docker__title">我的</div>
+    </div>
+  </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.docker {
+  border-top: 1px solid #f1f1f1;
+  // background-color: lightgreen;
+  padding-top: 0.03rem;
+  width: 100%;
+  height: 0.55rem;
+  padding-left: 0.18rem;
+  padding-right: 0.18rem;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  display: flex;
+
+  &__item {
+    // border: 1px solid red;
+    flex-grow: 1;
+    font-size: 0.12rem;
+    text-align: center;
+
+    .iconfont {
+      font-size: 0.28rem;
+    }
+
+    &--active {
+      color: #07ac25;
+    }
+  }
+}
+</style>
