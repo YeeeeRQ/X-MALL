21b8b917d9ace50974620d9219e792b8ca0fcd4f
Wox <YeeeeRQ@outlook.com>
2022-06-29 10:48:57 +0800

    样式添加:   首页-搜索框   首页-广告banner, 图片在慢网速下防抖

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,18 @@
       >
       <span class="iconfont icon-bell"></span>
     </div>
+    <div class="search">
+      <span class="iconfont icon-fenxiang"></span>
+      <!-- <input type="text" class="search__keywords" /> -->
+      <span class="search__keywords">永辉会员商店优惠商品</span>
+    </div>
+    <div class="banner">
+      <img
+        src="//img12.360buyimg.com/img/s750x205_jfs/t1/126041/16/23560/25488/62b29ffdE10c1ab86/9463e6b699976153.jpg!q70.dpg.webp"
+        alt="banner img"
+      />
+    </div>
+    123
   </div>
   <div class="docker">
     <div class="docker__item docker__item--active">
@@ -42,6 +54,38 @@
   right: 0;
   padding-left: 0.18rem;
   padding-right: 0.18rem;
+
+  .banner {
+    height: 0;
+    overflow: hidden;
+    padding-bottom: 27.3%;
+  }
+
+  .search {
+    // border: 1px solid red;
+    color: #b7b7b7;
+    background-color: #f5f5f5;
+    border-radius: 0.2rem;
+    font-size: 0.16rem;
+    line-height: 0.32rem;
+    margin-top: 0.08rem;
+    margin-bottom: 0.12rem;
+    padding-top: 0.03rem;
+    padding-bottom: 0.03rem;
+
+    display: flex;
+    > .iconfont {
+      margin-left: 0.12rem;
+      margin-right: 0.12rem;
+    }
+
+    &__keywords {
+      // border: 1px solid black;
+      flex-grow: 1;
+      margin-right: 0.12rem;
+      @include ellipsis;
+    }
+  }
 
   // background-color: lightgoldenrodyellow;
   .position {
